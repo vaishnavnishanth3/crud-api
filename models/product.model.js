4ac5b1c2c831736d9da7433472fe8c4a2ce90d16
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-17 20:50:08 +0530

    New Changes

--- models/product.model.js
+++ models/product.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+    
 const ProductSchema = mongoose.Schema(
     {
         name: {
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import Product from './models/product.model.js';
+import axios from "axios"
 
 dotenv.config();
 
 const app = express();
 const port = 3001;
+
+app.use(express.json());
 
 const connectionString = process.env.MONGODB_URI;
 
@@ -20,6 +24,20 @@ mongoose.connect(connectionString,
 app.get('/', (req, res) => {
     res.send();
 });
+
+app.post('/api/products', async (req, res) => {
+    try
+    {
+        const product = await Product.create(req.body)
+        console.log(product)
+        res.status(200).json(product);
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message});
+        console.log(error);
+    }
+})
 
 app.listen(port, () => {
     console.log(`\nServer is running on http://localhost:${port}`);
