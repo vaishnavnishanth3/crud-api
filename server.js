87606e58243c3d1a006f7b9c57ab13f3202f86cd
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-17 14:25:30 +0530

    Model Added

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 import express from 'express';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const port = 3001;
+
+const connectionString = process.env.MONGODB_URI;
+
+mongoose.connect(connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to database\n"))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send();
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}\nhttp://localhost:${port}`);
+    console.log(`\nServer is running on http://localhost:${port}`);
 });
